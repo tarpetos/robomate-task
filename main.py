2d1b6d4ac674e9b27e31e3472954c0e64c3e1167
Taras Hevlich <74049403+tarpetos@users.noreply.github.com>
2023-08-28 20:03:53 +0300

    Add OOP features (#1)
    * Add OOP features
    
    * Fix apply counter checker
    
    * Add runner
    
    * Fix logger output
    
    * Add tests

--- main.py
+++ main.py
@@ -1,99 +1,156 @@
+import logging
 import os
 import time
-from typing import List, Dict
+from abc import ABC, abstractmethod
+from itertools import islice, cycle
+from typing import List, Dict, Any, Optional
 
 import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-class RPAParser:
-    DOWNLOADS_DIR = 'downloads'
-    DATA_FILE_NAME = 'challenge.xlsx'
-    PARSE_URL = 'https://rpachallenge.com/'
+DOWNLOADS_DIR = "downloads"
+DATA_FILE_NAME = "challenge.xlsx"
+PARSE_URL = "https://rpachallenge.com/"
 
-    def __init__(self):
-        self.chrome_options = webdriver.ChromeOptions()
-        self.prefs = {'download.default_directory': self.downloads_path_checker()}
-        self.chrome_options.add_experimental_option('prefs', self.prefs)
-        self.driver = webdriver.Chrome(options=self.chrome_options)
-        self.driver.get(self.PARSE_URL)
 
-    def make_click_on_specific_element(self, xpath: str, download_queue: bool = False) -> None:
-        element = self.driver.find_element(By.XPATH, xpath)
-        element.click()
-        if download_queue:
-            self.wait_for_data()
+class Worker(ABC):
+    def open_url(self, driver: webdriver.Chrome) -> Any:
+        driver.get(PARSE_URL)
 
-    def solve_challenge(self) -> None:
+    @abstractmethod
+    def do_action(self, *args, **kwargs) -> Any:
+        pass
+
+
+class XlsxHandler(Worker):
+    def __init__(
+        self, downloads_dir=DOWNLOADS_DIR, data_file_name=DATA_FILE_NAME, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.output = None
+        self.downloads_dir = downloads_dir
+        self.data_file_name = data_file_name
+
+    def open_url(self, driver: webdriver.Chrome):
         download_button_xpath = '//a[@href="./assets/downloadFiles/challenge.xlsx"]'
-        start_challenge_button_xpath = '//button[contains(@class, "btn")]'
-        submit_page_xpath = '//input[contains(@class, "btn")]'
+        element = driver.find_element(By.XPATH, download_button_xpath)
+        element.click()
+        self.wait_for_data()
 
-        self.make_click_on_specific_element(download_button_xpath, download_queue=True)
-        self.make_click_on_specific_element(start_challenge_button_xpath)
-        insertion_data = self.parse_excel(self.DOWNLOADS_DIR, self.DATA_FILE_NAME)
+    def do_action(self, *args) -> List[Dict[str, str]]:
+        file_path = os.path.join(self.downloads_dir, self.data_file_name)
 
-        if insertion_data is None:
-            return
-
-        input_mapping = {
-            'Address': 'labelAddress',
-            'Company Name': 'labelCompanyName',
-            'Phone Number': 'labelPhone',
-            'Last Name': 'labelLastName',
-            'Role in Company': 'labelRole',
-            'First Name': 'labelFirstName',
-            'Email': 'labelEmail'
-        }
-
-        for user_data in insertion_data:
-            self.insert_data(user_data, input_mapping)
-            self.make_click_on_specific_element(submit_page_xpath)
-
-        time.sleep(5)
-
-    def insert_data(self, user_data: Dict[str, str], input_mapping: Dict[str, str]):
-        for attribute, value in user_data.items():
-            user_data_element_xpath = f'//input[@ng-reflect-name="{input_mapping[attribute]}"]'
-            input_element = self.driver.find_element(By.XPATH, user_data_element_xpath)
-            input_element.send_keys(value)
-
-    def downloads_path_checker(self) -> str:
-        project_path = os.path.dirname(os.path.abspath(__file__))
-        downloads_folder = os.path.join(project_path, self.DOWNLOADS_DIR)
-
-        existing_file_path = os.path.join(downloads_folder, self.DATA_FILE_NAME)
-        if os.path.exists(existing_file_path):
-            os.remove(existing_file_path)
-
-        return downloads_folder
-
-    def wait_for_data(self) -> None:
-        while not os.path.exists(os.path.join(self.DOWNLOADS_DIR, self.DATA_FILE_NAME)):
-            time.sleep(1)
-
-    @staticmethod
-    def parse_excel(dirname: str, filename: str) -> List[Dict[str, str]] | None:
-        file_path = os.path.join(dirname, filename)
-        result_dict = None
-
-        try:
-            data = pd.ExcelFile(file_path)
-            df = data.parse(data.sheet_names[0])
-            df.columns = df.columns.str.strip()
-            result_dict = df.to_dict(orient='records')
-        except FileNotFoundError:
-            print('Could not find the data file. Check the download path and try again.')
+        data = pd.ExcelFile(file_path)
+        df = data.parse(data.sheet_names[0])
+        df.columns = df.columns.str.strip()
+        result_dict = df.to_dict(orient="records")
 
         return result_dict
 
+    @staticmethod
+    def wait_for_data() -> None:
+        while not os.path.exists(os.path.join(DOWNLOADS_DIR, DATA_FILE_NAME)):
+            time.sleep(1)
 
-def main():
-    challenge_solver = RPAParser()
-    challenge_solver.solve_challenge()
+
+class FormFiller(Worker):
+    def __init__(self):
+        self.input_mapping = {
+            "Address": "labelAddress",
+            "Company Name": "labelCompanyName",
+            "Phone Number": "labelPhone",
+            "Last Name": "labelLastName",
+            "Role in Company": "labelRole",
+            "First Name": "labelFirstName",
+            "Email": "labelEmail",
+        }
+
+    def open_url(self, driver: webdriver.Chrome) -> None:
+        super().open_url(driver)
+        start_challenge_button_xpath = '//button[contains(@class, "btn")]'
+        element = driver.find_element(By.XPATH, start_challenge_button_xpath)
+        element.click()
+
+    def do_action(
+        self,
+        driver: webdriver.Chrome,
+        insertion_data: Optional[List[Dict[str, str]]],
+        form_apply_count: int,
+    ) -> None:
+        if not insertion_data:
+            raise ValueError("No insertion data provided")
+        insertion_data = self.expand_to_length(insertion_data, form_apply_count)
+
+        submit_page_xpath = '//input[contains(@class, "btn")]'
+        for counter, user_data in enumerate(insertion_data, 1):
+            logger.info("Inserting data for the %d worker...", counter)
+            self.insert_data(user_data, self.input_mapping, driver)
+            element = driver.find_element(By.XPATH, submit_page_xpath)
+            element.click()
+        logger.info("Form was filled successfully %d time (s)!", len(insertion_data))
+
+    @staticmethod
+    def expand_to_length(
+        insertion_data: List[Dict[str, str]], x: int
+    ) -> List[Dict[str, Any]]:
+        return list(islice(cycle(insertion_data), x))
+
+    @staticmethod
+    def insert_data(
+        user_data: Dict[str, str],
+        input_mapping: Dict[str, str],
+        driver: webdriver.Chrome,
+    ) -> None:
+        for attribute, value in user_data.items():
+            user_data_element_xpath = (
+                f'//input[@ng-reflect-name="{input_mapping[attribute]}"]'
+            )
+            input_element = driver.find_element(By.XPATH, user_data_element_xpath)
+            input_element.send_keys(value)
 
 
-if __name__ == '__main__':
+class Runner:
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+        self.driver.get(PARSE_URL)
+
+    def run(self, worker: Worker, **kwargs) -> None:
+        worker.open_url(driver=self.driver)
+        worker.output = worker.do_action(self.driver, **kwargs)
+
+
+def downloads_path_checker(
+    downloads_dir=DOWNLOADS_DIR, data_file_name=DATA_FILE_NAME
+) -> str:
+    project_path = os.path.dirname(os.path.abspath(__file__))
+    downloads_folder = os.path.join(project_path, downloads_dir)
+
+    existing_file_path = os.path.join(downloads_folder, data_file_name)
+    if os.path.exists(existing_file_path):
+        os.remove(existing_file_path)
+
+    return downloads_folder
+
+
+def main() -> None:
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": downloads_path_checker()}
+    chrome_options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=chrome_options)
+
+    runner = Runner(driver)
+    xlsx_worker = XlsxHandler()
+    form_worker = FormFiller()
+
+    runner.run(xlsx_worker)
+    data = xlsx_worker.output
+    runner.run(form_worker, insertion_data=data, form_apply_count=5)
+
+
+if __name__ == "__main__":
     main()
---
+++ tests/test_main.py
@@ -0,0 +1,207 @@
+import os
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from main import XlsxHandler, FormFiller, Runner, downloads_path_checker
+
+from unittest.mock import Mock, patch, create_autospec
+
+
+@pytest.fixture
+def insertion_data():
+    return [
+        {
+            "First Name": "John",
+            "Last Name": "Smith",
+            "Company Name": "IT Solutions",
+            "Role in Company": "Analyst",
+            "Address": "98 North Road",
+            "Email": "jsmith@itsolutions.co.uk",
+            "Phone Number": 40716543298,
+        },
+        {
+            "First Name": "Jane",
+            "Last Name": "Dorsey",
+            "Company Name": "MediCare",
+            "Role in Company": "Medical Engineer",
+            "Address": "11 Crown Street",
+            "Email": "jdorsey@mc.com",
+            "Phone Number": 40791345621,
+        },
+        {
+            "First Name": "Albert",
+            "Last Name": "Kipling",
+            "Company Name": "Waterfront",
+            "Role in Company": "Accountant",
+            "Address": "22 Guild Street",
+            "Email": "kipling@waterfront.com",
+            "Phone Number": 40735416854,
+        },
+        {
+            "First Name": "Michael",
+            "Last Name": "Robertson",
+            "Company Name": "MediCare",
+            "Role in Company": "IT Specialist",
+            "Address": "17 Farburn Terrace",
+            "Email": "mrobertson@mc.com",
+            "Phone Number": 40733652145,
+        },
+        {
+            "First Name": "Doug",
+            "Last Name": "Derrick",
+            "Company Name": "Timepath Inc.",
+            "Role in Company": "Analyst",
+            "Address": "99 Shire Oak Road",
+            "Email": "dderrick@timepath.co.uk",
+            "Phone Number": 40799885412,
+        },
+        {
+            "First Name": "Jessie",
+            "Last Name": "Marlowe",
+            "Company Name": "Aperture Inc.",
+            "Role in Company": "Scientist",
+            "Address": "27 Cheshire Street",
+            "Email": "jmarlowe@aperture.us",
+            "Phone Number": 40733154268,
+        },
+        {
+            "First Name": "Stan",
+            "Last Name": "Hamm",
+            "Company Name": "Sugarwell",
+            "Role in Company": "Advisor",
+            "Address": "10 Dam Road",
+            "Email": "shamm@sugarwell.org",
+            "Phone Number": 40712462257,
+        },
+        {
+            "First Name": "Michelle",
+            "Last Name": "Norton",
+            "Company Name": "Aperture Inc.",
+            "Role in Company": "Scientist",
+            "Address": "13 White Rabbit Street",
+            "Email": "mnorton@aperture.us",
+            "Phone Number": 40731254562,
+        },
+        {
+            "First Name": "Stacy",
+            "Last Name": "Shelby",
+            "Company Name": "TechDev",
+            "Role in Company": "HR Manager",
+            "Address": "19 Pineapple Boulevard",
+            "Email": "sshelby@techdev.com",
+            "Phone Number": 40741785214,
+        },
+        {
+            "First Name": "Lara",
+            "Last Name": "Palmer",
+            "Company Name": "Timepath Inc.",
+            "Role in Company": "Programmer",
+            "Address": "87 Orange Street",
+            "Email": "lpalmer@timepath.co.uk",
+            "Phone Number": 40731653845,
+        },
+    ]
+
+
+@pytest.fixture
+def driver():
+    mock_driver = create_autospec(webdriver.Chrome)
+    return mock_driver
+
+
+@pytest.fixture
+def xlsx_handler():
+    return XlsxHandler()
+
+
+@pytest.fixture
+def form_filler():
+    return FormFiller()
+
+
+@pytest.fixture
+def runner(driver):
+    return Runner(driver)
+
+
+def test_xlsx_handler_open_url(driver, xlsx_handler):
+    mock_element = Mock()
+    mock_element.click = Mock()
+    driver.find_element.return_value = mock_element
+
+    with patch.object(xlsx_handler, "wait_for_data", return_value=None):
+        xlsx_handler.open_url(driver)
+        driver.find_element.assert_called_once_with(
+            By.XPATH, '//a[@href="./assets/downloadFiles/challenge.xlsx"]'
+        )
+        mock_element.click.assert_called_once()
+        xlsx_handler.wait_for_data.assert_called_once()
+
+
+def test_xlsx_handler_do_action_success(insertion_data):
+    xlsx_handler = XlsxHandler(
+        downloads_dir="tests/", data_file_name="test_challenge.xlsx"
+    )
+    actual_result = xlsx_handler.do_action()
+    expected_result = insertion_data
+    assert actual_result == expected_result
+
+
+def test_xlsx_handler_do_action_fail():
+    with pytest.raises(FileNotFoundError):
+        xlsx_handler = XlsxHandler(
+            downloads_dir="tests/", data_file_name="fake_test_challenge.xlsx"
+        )
+        xlsx_handler.do_action()
+
+
+def test_form_filler_open_url(driver, form_filler):
+    mock_element = Mock()
+    mock_element.click = Mock()
+    driver.find_element.return_value = mock_element
+
+    form_filler.open_url(driver)
+    driver.find_element.assert_called_once_with(
+        By.XPATH, '//button[contains(@class, "btn")]'
+    )
+    mock_element.click.assert_called_once()
+
+
+def test_form_filler_do_action_success(driver, form_filler, insertion_data):
+    mock_element = Mock()
+    mock_element.click = Mock()
+    driver.find_element.return_value = mock_element
+
+    with patch.object(form_filler, "expand_to_length", return_value=insertion_data):
+        form_filler.do_action(
+            driver, insertion_data=insertion_data, form_apply_count=10
+        )
+        form_filler.expand_to_length.assert_called_once()
+        assert mock_element.click.call_count == 10
+
+
+def test_form_filler_do_action_fail(driver, form_filler):
+    with pytest.raises(ValueError):
+        form_filler = FormFiller()
+        form_filler.do_action(driver, insertion_data=None, form_apply_count=5)
+
+
+def test_runner_run(driver, runner):
+    mock_worker = Mock()
+    mock_worker.open_url = Mock()
+    mock_worker.do_action = Mock()
+    runner.run(mock_worker)
+
+    mock_worker.open_url.assert_called_once()
+    mock_worker.do_action.assert_called_once()
+
+
+def test_downloads_path_checker():
+    project_path = os.path.dirname(os.path.abspath("tests"))
+    expected_result = os.path.join(project_path, "tests")
+    actual_result = downloads_path_checker(
+        downloads_dir="tests", data_file_name="test_remove_challenge.xlsx"
+    )
+    assert actual_result == expected_result
