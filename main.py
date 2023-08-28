3abcd8268503dfe1f8817273070a7826db706036
tarpetos <hevlichtaras@gmail.com>
2023-08-28 20:01:44 +0300

    Add tests

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import logging
 import os
 import time
 from abc import ABC, abstractmethod
-from typing import List, Dict, Any
+from itertools import islice, cycle
+from typing import List, Dict, Any, Optional
 
 import pandas as pd
 
@@ -12,22 +13,28 @@ from selenium.webdriver.common.by import By
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-DOWNLOADS_DIR = 'downloads'
-DATA_FILE_NAME = 'challenge.xlsx'
-PARSE_URL = 'https://rpachallenge.com/'
+DOWNLOADS_DIR = "downloads"
+DATA_FILE_NAME = "challenge.xlsx"
+PARSE_URL = "https://rpachallenge.com/"
 
 
 class Worker(ABC):
-    @abstractmethod
     def open_url(self, driver: webdriver.Chrome) -> Any:
-        pass
+        driver.get(PARSE_URL)
 
     @abstractmethod
-    def do_action(self, *args) -> Any:
+    def do_action(self, *args, **kwargs) -> Any:
         pass
 
 
 class XlsxHandler(Worker):
+    def __init__(
+        self, downloads_dir=DOWNLOADS_DIR, data_file_name=DATA_FILE_NAME, **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.output = None
+        self.downloads_dir = downloads_dir
+        self.data_file_name = data_file_name
 
     def open_url(self, driver: webdriver.Chrome):
         download_button_xpath = '//a[@href="./assets/downloadFiles/challenge.xlsx"]'
@@ -35,73 +42,76 @@ class XlsxHandler(Worker):
         element.click()
         self.wait_for_data()
 
-    def do_action(self, *args) -> List[Dict[str, str]] | None:
-        file_path = os.path.join(DOWNLOADS_DIR, DATA_FILE_NAME)
-        result_dict = None
+    def do_action(self, *args) -> List[Dict[str, str]]:
+        file_path = os.path.join(self.downloads_dir, self.data_file_name)
 
-        try:
-            data = pd.ExcelFile(file_path)
-            df = data.parse(data.sheet_names[0])
-            df.columns = df.columns.str.strip()
-            result_dict = df.to_dict(orient='records')
-        except FileNotFoundError as e:
-            logger.info(e)
+        data = pd.ExcelFile(file_path)
+        df = data.parse(data.sheet_names[0])
+        df.columns = df.columns.str.strip()
+        result_dict = df.to_dict(orient="records")
 
         return result_dict
 
     @staticmethod
     def wait_for_data() -> None:
         while not os.path.exists(os.path.join(DOWNLOADS_DIR, DATA_FILE_NAME)):
-            time.sleep(2)
+            time.sleep(1)
 
 
 class FormFiller(Worker):
     def __init__(self):
         self.input_mapping = {
-            'Address': 'labelAddress',
-            'Company Name': 'labelCompanyName',
-            'Phone Number': 'labelPhone',
-            'Last Name': 'labelLastName',
-            'Role in Company': 'labelRole',
-            'First Name': 'labelFirstName',
-            'Email': 'labelEmail'
+            "Address": "labelAddress",
+            "Company Name": "labelCompanyName",
+            "Phone Number": "labelPhone",
+            "Last Name": "labelLastName",
+            "Role in Company": "labelRole",
+            "First Name": "labelFirstName",
+            "Email": "labelEmail",
         }
 
     def open_url(self, driver: webdriver.Chrome) -> None:
+        super().open_url(driver)
         start_challenge_button_xpath = '//button[contains(@class, "btn")]'
         element = driver.find_element(By.XPATH, start_challenge_button_xpath)
         element.click()
 
-    def do_action(self, insertion_data: List[Dict[str, str]], form_apply_count: int, driver: webdriver.Chrome) -> None:
-        if insertion_data is None:
-            return
+    def do_action(
+        self,
+        driver: webdriver.Chrome,
+        insertion_data: Optional[List[Dict[str, str]]],
+        form_apply_count: int,
+    ) -> None:
+        if not insertion_data:
+            raise ValueError("No insertion data provided")
+        insertion_data = self.expand_to_length(insertion_data, form_apply_count)
 
         submit_page_xpath = '//input[contains(@class, "btn")]'
         for counter, user_data in enumerate(insertion_data, 1):
-            counter_check = self.apply_counter_check(counter, form_apply_count, len(insertion_data))
-            if counter_check is None:
-                return
-
-            logger.info('Inserting data for the %d worker...', counter)
+            logger.info("Inserting data for the %d worker...", counter)
             self.insert_data(user_data, self.input_mapping, driver)
             element = driver.find_element(By.XPATH, submit_page_xpath)
             element.click()
+        logger.info("Form was filled successfully %d time (s)!", len(insertion_data))
 
     @staticmethod
-    def insert_data(user_data: Dict[str, str], input_mapping: Dict[str, str], driver: webdriver.Chrome) -> None:
+    def expand_to_length(
+        insertion_data: List[Dict[str, str]], x: int
+    ) -> List[Dict[str, Any]]:
+        return list(islice(cycle(insertion_data), x))
+
+    @staticmethod
+    def insert_data(
+        user_data: Dict[str, str],
+        input_mapping: Dict[str, str],
+        driver: webdriver.Chrome,
+    ) -> None:
         for attribute, value in user_data.items():
-            user_data_element_xpath = f'//input[@ng-reflect-name="{input_mapping[attribute]}"]'
+            user_data_element_xpath = (
+                f'//input[@ng-reflect-name="{input_mapping[attribute]}"]'
+            )
             input_element = driver.find_element(By.XPATH, user_data_element_xpath)
             input_element.send_keys(value)
-
-    @staticmethod
-    def apply_counter_check(loop_counter: int, form_apply_count: int, max_counter_value: int) -> None | str:
-        if form_apply_count < 0 or form_apply_count > max_counter_value:
-            logger.info(msg='Form cannot be filled. Invalid input!')
-        elif form_apply_count == loop_counter - 1:
-            logger.info('Form was filled successfully %d time (s)!', loop_counter - 1)
-        else:
-            return 'Loop continues'
 
 
 class Runner:
@@ -109,18 +119,18 @@ class Runner:
         self.driver = driver
         self.driver.get(PARSE_URL)
 
-    def run(
-            self, worker: Any, insertion_data: Dict[str, str] | None = None, form_apply_count: int = 0
-    ) -> Dict[str, str] | None:
+    def run(self, worker: Worker, **kwargs) -> None:
         worker.open_url(driver=self.driver)
-        return worker.do_action(insertion_data, form_apply_count, self.driver)
+        worker.output = worker.do_action(self.driver, **kwargs)
 
 
-def downloads_path_checker() -> str:
+def downloads_path_checker(
+    downloads_dir=DOWNLOADS_DIR, data_file_name=DATA_FILE_NAME
+) -> str:
     project_path = os.path.dirname(os.path.abspath(__file__))
-    downloads_folder = os.path.join(project_path, DOWNLOADS_DIR)
+    downloads_folder = os.path.join(project_path, downloads_dir)
 
-    existing_file_path = os.path.join(downloads_folder, DATA_FILE_NAME)
+    existing_file_path = os.path.join(downloads_folder, data_file_name)
     if os.path.exists(existing_file_path):
         os.remove(existing_file_path)
 
@@ -129,17 +139,18 @@ def downloads_path_checker() -> str:
 
 def main() -> None:
     chrome_options = webdriver.ChromeOptions()
-    prefs = {'download.default_directory': downloads_path_checker()}
-    chrome_options.add_experimental_option('prefs', prefs)
+    prefs = {"download.default_directory": downloads_path_checker()}
+    chrome_options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(options=chrome_options)
 
     runner = Runner(driver)
     xlsx_worker = XlsxHandler()
     form_worker = FormFiller()
 
-    data = runner.run(xlsx_worker)
+    runner.run(xlsx_worker)
+    data = xlsx_worker.output
     runner.run(form_worker, insertion_data=data, form_apply_count=5)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
